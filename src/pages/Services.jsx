64209c8f4a44e271f4a016e587be0f3a9a2dad97
paul <ampaulkaranja@gmail.com>
2025-08-23 22:28:10 +0300

    finalized the services landing page

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,9 +1,31 @@
 import React from "react";
+import Header from "../components/Header";
 
 const Services = () => {
     return(
-        <div>
-            <h1>This is the services page</h1>
+        <div className="bg-background">
+            {/* SECTION 1 */}
+            <div className="w-full h-[100vh] bg-primary flex flex-col items-center justify-between">
+                <Header/>
+                <div className="w-full h-7/8 flex flex-row items-center justify-center flex-wrap">
+                   <div className="w-1/2">
+                        <div className=" pl-4 border-l-1 border-background w-110">
+                             <p className="h-full text-5xl text-background">Services Designed to Build & <span className=" text-accent headerSpan"> Grow Your Business </span></p>
+                        </div>
+                        <br /><br /><br />
+                        <p className="text-sm text-background">Creating a powerful online presence by providing everything you need to succeed!</p>
+                   </div>
+                   <div className=" w-1/3 h-full flex flex-row items-center justify-center">
+                      <div className="w-60 h-70 bg-accent shadow-2xl rotate-30 rounded-2xl">
+                         <div className="w-60 h-70 bg-secondary -rotate-30 -translate-x-18 rounded-2xl shadow-2xl"></div>
+                      </div>
+                   </div>
+
+                </div>
+            </div>
+
+            <br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+
         </div>
     );
 
