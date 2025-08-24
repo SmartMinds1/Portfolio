5fbba7a0cd31cc87abd0044de428a8c466e732d1
paul <ampaulkaranja@gmail.com>
2025-08-24 21:57:34 +0300

    added overview services on service page

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -8,7 +8,7 @@ const Services = () => {
             <div className="w-full h-[100vh] bg-primary flex flex-col items-center justify-between">
                 <Header/>
                 <div className="w-full h-7/8 flex flex-row items-center justify-center flex-wrap">
-                   <div className="w-1/2">
+                   <div className="w-1/2 h-3/5">
                         <div className=" pl-4 border-l-1 border-background w-110">
                              <p className="h-full text-5xl text-background">Services Designed to Build & <span className=" text-accent headerSpan"> Grow Your Business </span></p>
                         </div>
@@ -17,10 +17,15 @@ const Services = () => {
                    </div>
                    <div className=" w-1/3 h-full flex flex-row items-center justify-center">
                       <div className="w-60 h-70 bg-accent shadow-2xl rotate-30 rounded-2xl">
-                         <div className="w-60 h-70 bg-secondary -rotate-30 -translate-x-18 rounded-2xl shadow-2xl"></div>
+                         <div className="w-60 h-70 bg-secondary -rotate-30 -translate-x-18 rounded-2xl shadow-2xl p-4">
+                            <ul className="flex flex-col items-start justify-evenly h-full text-xl  list-disc p-4">
+                                <li className="text-background">Web Development</li>
+                                <li>Google Ads Management</li>
+                                <li className="text-background">Analytics & Optimization</li>
+                            </ul>
+                         </div>
                       </div>
                    </div>
-
                 </div>
             </div>
 
