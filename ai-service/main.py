0f71f10d7e6356a10cba9da9b50622aaa9a41aeb
paul <ampaulkaranja@gmail.com>
2026-05-18 16:47:18 +0300

    Added the requirements.txt file

--- ai-service/main.py
+++ ai-service/main.py
@@ -12,7 +12,10 @@ app = FastAPI()
 # CORS setup
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://biznutritia.vercel.app"
+        ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
